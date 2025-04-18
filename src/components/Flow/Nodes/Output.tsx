38862a5a6d8e6164d158c39aad952faa6459ab5d
MrPoll0 <mrp0ollo@gmail.com>
2025-04-18 22:10:06 +0200

    feat: Output basic processor

--- src/components/Flow/Nodes/Output.tsx
+++ src/components/Flow/Nodes/Output.tsx
@@ -1,11 +1,45 @@
-import { Handle, Position } from '@xyflow/react';
-import { memo } from 'react';
+import { Handle, Position, ReactFlowInstance, useReactFlow } from '@xyflow/react';
+import { memo, useEffect } from 'react';
 import { getNodeStyles } from '../../../utils/nodeStyles';
-import { BaseNode, } from './NodeTypes';
-import { Expression } from '../../../models';
+import { BaseNode, NodeProcessor, } from './NodeTypes';
+import { Expression, VariableType } from '../../../models';
+import { IValuedVariable } from '../../../models/ValuedVariable';
+import { ValuedVariable } from '../../../models/ValuedVariable';
 
 interface OutputNode extends BaseNode {
   expression?: Expression;
+}
+
+class OutputProcessor implements NodeProcessor {
+  constructor(private reactFlow: ReactFlowInstance, private nodeId: string) {}
+  
+  process(): ValuedVariable<VariableType>[] {
+    const node = this.reactFlow.getNode(this.nodeId)!;
+    const data = node.data as OutputNode;
+    console.log(`Processing Output node ${this.nodeId} with expression:`, data.expression);
+
+    let currentValuedVariables: ValuedVariable<VariableType>[] = [];
+    data.currentValuedVariables?.forEach((valuedVariable: IValuedVariable<VariableType>) => {
+      currentValuedVariables.push(ValuedVariable.fromObject(valuedVariable));
+    });
+
+    console.log("Current valued variables:");
+    currentValuedVariables.forEach((valuedVariable: ValuedVariable<VariableType>) => {
+      console.log(valuedVariable.toString());
+    });
+
+    // TODO: output sanitization for XSS attacks?
+
+    if(data.expression) {
+      const expr = data.expression && !(data.expression instanceof Expression) ? Expression.fromObject(data.expression) : null;
+      if(expr) {
+        const value = expr.calculateValue(expr.rightSide, null, currentValuedVariables);
+        console.log(`Output value: ${value}`);
+      }
+    }
+
+    return currentValuedVariables;
+  }
 }
 
 const Output = memo(function OutputComponent({ data, id: nodeId }: { data: OutputNode; id: string }) {
@@ -14,6 +48,25 @@ const Output = memo(function OutputComponent({ data, id: nodeId }: { data: Outpu
   // TODO: expression is not Expression but ExpressionElement[]
   // or just Expression without leftSide? (setup in constructor)
   // but it may be an extension of Expression or somewhat because it can also have string concatenation, commas, etc (e.g. variable1, variable2?)
+
+  const reactFlow = useReactFlow();
+
+  // Update processor only on mount/unmount to prevent infinite loops
+  useEffect(() => {
+    const processor = new OutputProcessor(reactFlow, nodeId);
+
+    // Set the processor to the node data to make it available for the flow executor to use 
+    reactFlow.updateNodeData(nodeId, {
+      processor: processor
+    });
+
+    // Clean up on unmount
+    return () => {
+      reactFlow.updateNodeData(nodeId, {
+        processor: null
+      });
+    };
+  }, [nodeId, reactFlow]);
 
   const expr = expression && !(expression instanceof Expression) ? Expression.fromObject(expression) : null;
 
